af14d64d8f7b1f66343e30e5928510e84ef49def
Victor <victor456472@gmail.com>
2024-10-08 15:09:16 -0500

    añadiendo archivos del proyecto

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+/*
+
+esto es un codigo modificado por jose
+
+*/
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
